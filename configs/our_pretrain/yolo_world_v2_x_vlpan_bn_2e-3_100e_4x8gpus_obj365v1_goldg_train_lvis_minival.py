fc041b9dd6676fa62f5cf64eafba16ee4ae36787
pokabu1999 <thang15021999@gmail.com>
2025-06-26 16:24:13 +0700

    fix: update code

--- configs/our_pretrain/yolo_world_v2_x_vlpan_bn_2e-3_100e_4x8gpus_obj365v1_goldg_train_lvis_minival.py
+++ configs/our_pretrain/yolo_world_v2_x_vlpan_bn_2e-3_100e_4x8gpus_obj365v1_goldg_train_lvis_minival.py
@@ -121,7 +121,7 @@ coco_val_dataset = dict(
     _delete_=True,
     type='MultiModalDataset',
     dataset=dict(type='YOLOv5LVISV1Dataset',
-                 data_root='data/coco/',
+                 data_root='content/YOLO-World/data/coco/',
                  test_mode=True,
                  ann_file='lvis/lvis_v1_minival_inserted_image_name.json',
                  data_prefix=dict(img=''),
@@ -132,7 +132,7 @@ val_dataloader = dict(dataset=coco_val_dataset)
 test_dataloader = val_dataloader
 
 val_evaluator = dict(type='mmdet.LVISMetric',
-                     ann_file='data/coco/lvis/lvis_v1_minival_inserted_image_name.json',
+                     ann_file='content/YOLO-World/data/coco/lvis/lvis_v1_minival_inserted_image_name.json',
                      metric='bbox')
 test_evaluator = val_evaluator
 
