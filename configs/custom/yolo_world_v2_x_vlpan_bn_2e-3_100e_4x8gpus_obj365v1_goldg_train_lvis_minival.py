0aaf5363b86197c4af9eb719ccd4fbab627355ce
pokabu1999 <thang15021999@gmail.com>
2025-07-03 23:11:06 +0700

    fix: update code

--- configs/custom/yolo_world_v2_x_vlpan_bn_2e-3_100e_4x8gpus_obj365v1_goldg_train_lvis_minival.py
+++ configs/custom/yolo_world_v2_x_vlpan_bn_2e-3_100e_4x8gpus_obj365v1_goldg_train_lvis_minival.py
@@ -136,7 +136,7 @@ coco_val_dataset = dict(
                  ann_file='lvis/lvis_v1_minival_inserted_image_name.json',
                  data_prefix=dict(img=''),
                  batch_shapes_cfg=None),
-    class_text_path='data/texts/lvis_v1_class_texts.json',
+    class_text_path='/kaggle/input/grounding/data/texts/lvis_v1_class_texts.json',
     pipeline=test_pipeline)
 val_dataloader = dict(dataset=coco_val_dataset)
 test_dataloader = val_dataloader
