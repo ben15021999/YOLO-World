69ac36713d0fc8fc9bf7f9b370d799a64ff17925
pokabu1999 <thang15021999@gmail.com>
2025-07-03 22:25:52 +0700

    fix: update code

--- yolo_world/datasets/yolov5_custom_mixed_grounding.py
+++ yolo_world/datasets/yolov5_custom_mixed_grounding.py
@@ -145,7 +145,7 @@ class YOLOv5CustomMixedGroundingDataset(BatchShapePolicyDataset, CocoDataset):
         cat2id = {}
         texts = []
         for ann in ann_info:
-            cat_name = ' '.join([img_info['phrase']])
+            cat_name = ' '.join([info['phrase'] for info in ann['phrase']])
             if cat_name not in cat2id:
                 cat2id[cat_name] = len(cat2id)
                 texts.append([cat_name])
